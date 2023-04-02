b2b58a911ed9a0c6d68b26eaf3dda90618dcc600
unknown <lisandro.batiancila@gmail.com>
2023-04-02 12:44:00 +0800

    display properties jewelry / vehicle

--- server/mobile-controller/mobile-my-properties.js
+++ server/mobile-controller/mobile-my-properties.js
@@ -19,7 +19,7 @@ router.route("/mobile-my-vehicle-properties")
             AND c.userID =? AND c.propertyType = 'vehicle'",
                 querydata = [userID];
 
-            mysqlController.selectQuery(mysqlConn.vehicles_table, query, querydata, `${GLOBAL_FILE_NAME}-> ${GLOBAL_FUNCTION_NAME}-> ${lastAlgo}`, (response) => {
+            mysqlController.selectQuery(mysqlConn.vehicles_table, query, querydata, [GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, lastAlgo], (response) => {
                 lastAlgo = "@MVP3";
                 if(response != "error") {
                     responseObj = serverResonses.serverResponse(200);
@@ -30,12 +30,45 @@ router.route("/mobile-my-vehicle-properties")
                 else {
                     common.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `Error getting vehicle recrods-> ${lastAlgo}`);
                     responseObj = serverResonses.serverResponse(500);
+                    responseObj.myVehicleLists = [];
                     res.json(responseObj);
                 }
             })
         }
         catch(error) {
             common.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `${lastAlgo}-${error}`);
+            responseObj = serverResonses.serverResponse(500);
+            res.json(responseObj);
+        }
+    });
+
+
+router.route("/mobile-my-jewelry-properties")
+    .post((req, res) => {
+        const GLOBAL_FUNCTION_NAME = "mobileJewelryProperties()";
+        var lastAlgo = "",
+            responseObj = {};
+        try{
+            const { userID } = req.body;
+            const query = "SELECT a.*, b.jewelryIMG, c.assumptionCount, c.propertyStatus FROM jewelries a INNER JOIN jewelry_images b ON a.jewelryID = b.jewelryID INNER JOIN properties c ON c.propertyID = a.propertyID AND c.propertyType = 'jewelry' AND c.userID = ?",
+                querydata = [userID];
+            
+            mysqlController.selectQuery(mysqlConn.jewelries_table, query, querydata, [GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, lastAlgo], (response) => {
+                if(response != "error") {
+                    responseObj = serverResonses.serverResponse(200);
+                    responseObj.myJewelryLists = response;
+
+                    res.json(responseObj);
+                }
+                else {
+                    responseObj = serverResonses.serverResponse(500);
+                    responseObj.myJewelryLists = [];
+                    res.json(responseObj);
+                }
+            })
+        }
+        catch(error) {
+            common.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `${lastAlgo}-> ${error}`);
             responseObj = serverResonses.serverResponse(500);
             res.json(responseObj);
         }
