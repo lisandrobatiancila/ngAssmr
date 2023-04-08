00c918dfc86e2662547dc7be53ef710d20c0d885
unknown <lisandro.batiancila@gmail.com>
2023-04-09 03:20:39 +0800

    my properties VUR generic

--- server/mobile-controller/mobile-remove-properties.js
+++ server/mobile-controller/mobile-remove-properties.js
@@ -47,4 +47,59 @@ router.route("/mobile-remove-vehicle")
         }
     })
 
+router.route("/mobile-remove-realestates")
+    .post((req, res) => {
+        const GLOBAL_FUNCTION_NAME = "mobileRemoveRealestates()";
+        var lastAlgo = "@MRR1",
+            responseObj = {};
+        try{
+            const { propertyID } = req.body;
+            const query = "UPDATE properties set propertyStatus = 'removed' WHERE propertyID =?",
+                querydata = [propertyID];
+
+            mysqlController.updateQuery(mysqlConn.properties_table, query, querydata, [GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, lastAlgo], (response) => {
+                if(response != "error") {
+                    responseObj = serverResponse.serverResponse(200);
+                    res.json(responseObj);
+                }
+                else {
+                    responseObj = serverResponse.serverResponse(500);
+                    res.json(responseObj);
+                }
+            });
+        }
+        catch(error) {
+            commonLib.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `${lastAlgo}-${error}`);
+            responseObj = serverResponse.serverResponse(500);
+            res.json(responseObj);
+        }
+    });
+
+router.route("/mobile-remove-jewelry")
+    .post((req, res) => {
+        const GLOBAL_FUNCTION_NAME = "mobileRemoveJewelry()";
+        var lastAlgo = "@MRJ1",
+            responseObj = {};
+        try{
+            const { propertyID } = req.body;
+            const query = "UPDATE properties SET propertyStatus ='removed' WHERE propertyID =?",
+                querydata = [propertyID];
+            mysqlController.updateQuery(mysqlConn.properties_table, query, querydata, [GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, lastAlgo], (response) => {
+                if(response != "error") {
+                    responseObj = serverResponse.serverResponse(200);
+                    res.json(responseObj);
+                }
+                else {
+                    responseObj = serverResponse.serverResponse(500);
+                    res.json(responseObj);
+                }
+            })
+        }
+        catch(error) {
+            commonLib.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `${lastAlgo}-${error}`);
+            responseObj = serverResponse.serverResponse(500);
+            res.json(responseObj);
+        }
+    });
+
 module.exports = router;
--- server/mobile-controller/mobile-update-property.js
+++ server/mobile-controller/mobile-update-property.js
@@ -37,4 +37,60 @@ router.route("/update-vehicle-property")
         }
     });
 
+router.route("/update-realestate-property")
+    .post((req, res) => {
+        const GLOBAL_FUNCTION_NAME = "updateRealestateProperty()";
+        var lastAlgo = "@URP1",
+            responseObj = {};
+        try{
+            const { propertyID, owner, contactno, location, downpayment, paid, duration, delinquent, description } = req.body;
+            const query = "UPDATE realestates SET realestateOwner =?, realestateContactno =?, realestateLocation =?, realestateDownpayment =?, realestateInstallmentpaid =?, realestateInstallmentduration =?, realestateDelinquent =?, realestateDescription =? WHERE propertyID =?",
+                querydata = [owner, contactno, location, downpayment, paid, duration, delinquent, description, propertyID];
+            mysqlController.updateQuery(mysqlConn.realestates_table, query, querydata, [GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, lastAlgo], (response) => {
+                if(response != "error") {
+                    responseObj = serverResonses.serverResponse(200);
+                    res.json(responseObj);
+                }
+                else {
+                    responseObj = serverResonses.serverResponse(500);
+                    res.json(responseObj);
+                }
+            });
+        }
+        catch(error) {
+            commonlib.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `${lastAlgo}-${error}`);
+            responseObj = serverResonses.serverResponse(500);
+            res.json(responseObj);
+        }
+    });
+
+router.route("/update-jewelry-property")
+    .post((req, res) => {
+        const GLOBAL_FUNCTION_NAME = "updateJewelryProperty()";
+        var lastAlgo = "@UJP1",
+            responseObj = {};
+        try{
+            lastAlgo = "@UJP2";
+            const { owner, contactno, jname, jmodel, location, downpayment, installmentpaid, installmentduration, delinquent, description, propertyID } = req.body;
+            
+            const query = "UPDATE jewelries SET jewelryOwner =?, jewelryContactno =?, jewelryName =?, jewelryModel =?, jewelryLocation =?, jewelryDownpayment =?, jewelryInstallmentpaid =?, jewelryInstallmentduration =?, jewelryDelinquent =?, jewelryDescription = ? WHERE propertyID =?",
+                querydata = [owner, contactno, jname, jmodel, location, downpayment, installmentpaid, installmentduration, delinquent, description, propertyID];
+            mysqlController.updateQuery(mysqlConn.jewelries_table, query, querydata, [GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, lastAlgo], (respones) => {
+                if(respones != "error") {
+                    responseObj = serverResonses.serverResponse(200);
+                    res.json(responseObj);
+                }
+                else {
+                    responseObj = serverResonses.serverResponse(500);
+                    res.json(responseObj);
+                }
+            });
+        }
+        catch(error) {
+            commonlib.errorLogs(GLOBAL_FILE_NAME, GLOBAL_FUNCTION_NAME, `${lastAlgo}-${error}`);
+            responseObj = serverResonses.serverResponse(500);
+            res.json(responseObj);
+        }
+    });
+
 module.exports = router;
